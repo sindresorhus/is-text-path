56efb5e59b66e8589cb67c4e7585bac3bd9bc87f
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-25 13:30:46 +0200

    minor tweaks

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ var isTextPath = require('./');
 test(function (t) {
 	t.assert(isTextPath('unicorn.txt'));
 	t.assert(isTextPath('unicorn.md'));
+	t.assert(isTextPath('unicorn.MD'));
 	t.assert(!isTextPath('unicorn.png'));
 	t.assert(!isTextPath('unicorn.zip'));
 	t.assert(!isTextPath('unicornzip'));
